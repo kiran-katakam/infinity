595a5076063af1a07657528ab3b81fb2dc6d97cb
Dhanumjay Kiran Katakam <katakamkiran53@gmail.com>
2024-12-17 23:47:58 +0530

    scaffold moved to induvidual widgets

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
-import 'package:webview_flutter/webview_flutter.dart';
+import 'package:infinity/inputpage.dart';
+import 'package:infinity/webviewpage.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 void main() {
   runApp(MyApp());
 }
 
-//! implement algo
 class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
@@ -14,102 +15,36 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  late WebViewController controller;
+
+  late SharedPreferences data;
+  bool? isLoggedIn;
+  
+  Future<void> getData() async {
+    data = await SharedPreferences.getInstance();
+    setState(() {
+      isLoggedIn = data.containsKey("username");
+    });
+  }
 
   @override
   void initState() {
     super.initState();
-    controller = WebViewController()
-      ..setJavaScriptMode(JavaScriptMode.unrestricted)
-      ..setNavigationDelegate(
-        NavigationDelegate(
-          onProgress: (int progress) {},
-          onPageStarted: (String url) {},
-          onPageFinished: (String url) {
-            final String jsCode = "document.getElementById('ft_un').value = '²³BCE9783';document.getElementById('ft_pd').value = 'ejxFw4tr';document.forms[0].submit();";
-            controller.runJavaScript(jsCode);
-          },
-          onHttpError: (HttpResponseError error) {
-            print("HTTP Error: ${error.response}");
-          },
-          onWebResourceError: (WebResourceError error) {
-            print("Resource Error: ${error.description}");
-          },
-          onNavigationRequest: (NavigationRequest request) {
-            if (request.url.startsWith('https')) {
-              return NavigationDecision.prevent;
-            }
-            return NavigationDecision.navigate;
-          },
-        ),
-      )
-      ..loadRequest(Uri.parse('http://172.18.10.10:1000/logout?'));
+    getData();
   }
 
   @override
   Widget build(BuildContext context) {
+
+    if (isLoggedIn == null) {
+      return MaterialApp(
+        home: Scaffold(body: Center(child: CircularProgressIndicator())),
+      );
+    }
+
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       theme: ThemeData.dark(),
-      home: Scaffold(
-        body: WebViewWidget(controller: controller),
-      ),
+      home: isLoggedIn! ? Login() : Input()
     );
   }
 }
- 
-//! TODO 
-/*void main() {
-  List<String> usernameList = [];
-  usernameList.add("23BCE9783");
-  changeUsernameList(usernameList);
-  print(usernameList);
-}
-
-bool isNumeric(String s) {
-  if (s == null) {
-    return false;
-  }
-  return int.tryParse(s) != null;
-}
-
-void changeUsernameList(List<String> usernameList) {
-  final List<String> superscripts = [
-    "⁰",
-    "¹",
-    "²",
-    "³",
-    "⁴",
-    "⁵",
-    "⁶",
-    "⁷",
-    "⁸",
-    "⁹"
-  ];
-  var id = usernameList.elementAt(0);
-
-  for (int k = 0; k <= usernameList.length; k++) {
-    if (k < usernameList.length) {
-      id = usernameList.elementAt(k);
-    }
-    for (int i = 0; i < id.length; i++) {
-      var charid = id.split("");
-      if (isNumeric(charid[i])) {
-        charid[i] = superscripts[int.parse(charid[i])];
-        print(charid);
-        if (!usernameList.contains(toString(charid))) {
-          usernameList.add(toString(charid));
-        }
-      }
-      
-    }
-  }
-}
-
-String toString(List<String> list) {
-  String str = "";
-  for (int i = 0; i < list.length; i++) {
-    str += list[i];
-  }
-  return str;
-}
- */
